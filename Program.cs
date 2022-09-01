f2518c23fbb4627206b98de0a5cd9dfbc3a87e57
coolpagani <coolpagani@gmail.com>
2022-09-01 22:37:00 +0300

    Закончили работу над 19 задачей

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿string Reverse(int number)
+{
+    int[] array = number.ToString().Select(o=> Convert.ToInt32(o) - 48 ).ToArray();
+    string arrayReversed = String.Empty;
+    int length = array.Length;
+    for (int i = length; i > 0; i--)
+    {
+        arrayReversed += array[i-1];
+
+    }
+    return arrayReversed;
+}
+int num = 0;
+int.TryParse(Console.ReadLine(), out num);
+string checkNumber = Reverse(num);
+string num_string = "";
+num_string = num.ToString();
+
+string Check(string B)
+{
+    string a = "true";
+    string b = "false";
+    if (B == num_string)
+    return a;
+    else
+    return b;
+}
+
+Console.Write($"{num} -> {Reverse(num)} -> {Check(checkNumber)}");
