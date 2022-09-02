e82b0117607833f3e5d953ef8f1aef2347812641
coolpagani <coolpagani@gmail.com>
2022-09-02 15:03:25 +0300

    Закончили 23 задачу

--- Program.cs
+++ Program.cs
@@ -1,29 +1,15 @@
-﻿string Reverse(int number)
+﻿string Cube(int a)
 {
-    int[] array = number.ToString().Select(o=> Convert.ToInt32(o) - 48 ).ToArray();
-    string arrayReversed = String.Empty;
-    int length = array.Length;
-    for (int i = length; i > 0; i--)
+    double result = 0;
+    string numbers = "";
+    for (int i = 1; i <= a; i++)
     {
-        arrayReversed += array[i-1];
-
+    result = Math.Pow(i, 3);
+    numbers = numbers + result + " ";
     }
-    return arrayReversed;
+    return numbers;
 }
 int num = 0;
 int.TryParse(Console.ReadLine(), out num);
-string checkNumber = Reverse(num);
-string num_string = "";
-num_string = num.ToString();
 
-string Check(string B)
-{
-    string a = "true";
-    string b = "false";
-    if (B == num_string)
-    return a;
-    else
-    return b;
-}
-
-Console.Write($"{num} -> {Reverse(num)} -> {Check(checkNumber)}");
+Console.Write(Cube(num));
